2a3970d41d8ec1d90809d30fda48d6217a1b79fd
Jirokichi <topaz30th.kdy@gmail.com>
2018-02-20 01:03:00 +0900

    [ENH]material-uiの導入。試しにAppBarを利用。

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import AppHeader from './header/AppHeader';
 import ToDoListApp from './toDoListApp/ToDoListApp';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
 
   render() {
     return (
-      <div className="App">
-        <AppHeader />
-        <ToDoListApp />
-      </div>
+      // material-uiを使うときはトップにテーマ(MuiThemeProvider)を設定する
+      <MuiThemeProvider>
+        <div className="App">
+          <AppHeader />
+          <ToDoListApp />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/header/AppHeader.js
+++ src/header/AppHeader.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './AppHeader.css';
-
+import AppBar from 'material-ui/AppBar';
 class AppHeader extends Component {
     render() {
         return (
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">To Do List</h1>
-            </header>
+            // <header className="App-header">
+            //     <img src={logo} className="App-logo" alt="logo" />
+            //     <h1 className="App-title">To Do List</h1>
+            // </header>
+            // <MuiThemeProvider>
+            <AppBar
+                title="Title"
+                iconClassNameRight="muidocs-icon-navigation-expand-more"
+                onTitleClick={(event) => {
+                    console.log("onTitleClick")
+                }}
+            />
+            // </MuiThemeProvider>
         );
     }
 }
