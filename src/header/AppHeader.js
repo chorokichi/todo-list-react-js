32349f29535d894ed18757a38102d5012d74f3d6
Jirokichi <topaz30th.kdy@gmail.com>
2018-02-20 23:15:58 +0900

    [ENH]Drawerの追加(ReactとMaterial UIのリンク)

--- src/header/AppHeader.js
+++ src/header/AppHeader.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './AppHeader.css';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
 class AppHeader extends Component {
+    constructor() {
+        super();
+        this.state = {
+            open: false,
+            now: new Date()
+        };
+    }
+
+    chnageDrawerOpenStatus() {
+        this.setState({ open: !this.state.open })
+    }
+
     render() {
         return (
             // <header className="App-header">
@@ -10,13 +26,53 @@ class AppHeader extends Component {
             //     <h1 className="App-title">To Do List</h1>
             // </header>
             // <MuiThemeProvider>
-            <AppBar
-                title="Title"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-                onTitleClick={(event) => {
-                    console.log("onTitleClick")
-                }}
-            />
+            <div>
+                <AppBar
+                    title="Title"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onTitleClick={(event) => {
+                        console.log("onTitleClick")
+                    }}
+                    onLeftIconButtonClick={(event) => {
+                        console.log("onLeftIconButtonClick")
+                        this.chnageDrawerOpenStatus();
+                    }}
+                    iconElementLeft={<img src={logo} className="App-logo" alt="logo" />}
+                >
+
+                </AppBar>
+                <Drawer open={this.state.open}>
+                    <Menu onItemClick={(event, menuItem, index) => {
+                        console.log("On Item Click!");
+                        console.log(event);
+                        console.log(menuItem);
+                        console.log(index);
+                        this.chnageDrawerOpenStatus();
+                    }}>
+                        <MenuItem>
+                            {"" + Date.now()}
+                        </MenuItem>
+                        <MenuItem>
+                            <a href="https://reactjs.org/docs/hello-world.html"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                React Doc
+                            </a>
+                        </MenuItem>
+
+                        <MenuItem>
+                            <a href="http://www.material-ui.com/#/"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                Material UI Doc
+                            </a>
+                        </MenuItem>
+                    </Menu>
+                </Drawer>
+            </div>
+
             // </MuiThemeProvider>
         );
     }
