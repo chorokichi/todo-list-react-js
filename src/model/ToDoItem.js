99b3a33041cef3d65404c46c9ce211ea60c9a59e
Jirokichi <topaz30th.kdy@gmail.com>
2018-03-04 17:50:29 +0900

    [MOD]ToDoItem.js for flow

--- src/model/ToDoItem.js
+++ src/model/ToDoItem.js
@@ -1,5 +1,10 @@
+// @flow
+
 class ToDoItem {
-    constructor(id, value, createdOn) {
+    id: string;
+    value: string;
+    createdOn: Date;
+    constructor(id: string, value: string, createdOn: Date) {
         this.id = id;
         this.value = value;
         this.createdOn = createdOn;
