4ed34a28cc5a6db33ec0cdbed40bcb32a8a8dae9
Jirokichi <topaz30th.kdy@gmail.com>
2018-03-11 21:12:29 +0900

    Gitようわからん

--- src/toDoListApp/ToDoListApp.js
+++ src/toDoListApp/ToDoListApp.js
@@ -19,15 +19,17 @@ type Props = {
 type State = {
     resultDialog: { open: bool, title: string, message: string },
     textFieldValue: string,
-    values: TaskItem[]
+    values: TaskItem[],
+    tableSelectable: bool,
 };
 
 class ToDoListApp extends Component<Props, State> {
     constructor(props: Props) {
         super(props);
+        const now = new Date();
         const values = [
-            new TaskItem(outputId(), "test01", new Date()),
-            new TaskItem(outputId(), "test02", new Date())
+            new TaskItem(outputId(), "test01", now, now),
+            new TaskItem(outputId(), "test02", now, now)
         ]
 
         values.sort((a: TaskItem, b: TaskItem): number => {
@@ -43,11 +45,15 @@ class ToDoListApp extends Component<Props, State> {
                 message: ""
             },
             textFieldValue: "",
-            values: values
+            values: values,
+            tableSelectable: false
         };
         (this: any).onUpdateTextBox = this.onUpdateTextBox.bind(this);
         (this: any).onSubmit = this.onSubmit.bind(this);
         (this: any).updateSelectedStatus = this.updateSelectedStatus.bind(this);
+        (this: any).deleteSectedItems = this.deleteSectedItems.bind(this);
+        (this: any).deleteItem = this.deleteItem.bind(this);
+        (this: any).updateBulkOperationMode = this.updateBulkOperationMode.bind(this);
     }
 
     componentDidMount() {
@@ -96,7 +102,8 @@ class ToDoListApp extends Component<Props, State> {
             this.openDialog("結果", msg)
         }
 
-        const item = new TaskItem(outputId(), val, new Date());
+        const createdOn = new Date();
+        const item = new TaskItem(outputId(), val, createdOn, createdOn);
 
         // ToDoListTableがPureComponentのため、その中で利用するvaluesが別のオブジェクトになるように工夫している
         const newItems = [item, ...this.state.values]
@@ -108,52 +115,73 @@ class ToDoListApp extends Component<Props, State> {
         return true;
     }
 
-    updateSelectedStatus(selectedRows: number[]) {
-        let newItems: TaskItem[] = [];
-
+    updateSelectedStatus(selectedRows: number[] | 'all') {
+        const newItems: TaskItem[] = [];
         if (selectedRows === "all") {
             this.state.values.forEach((element: TaskItem, index: number) => {
-                element.closed = true
+                element.selected = true
                 newItems.push(element)
             })
         } else {
             for (var i = 0; i < this.state.values.length; i++) {
-                console.log("i: " + i)
                 let item = this.state.values[i];
                 if (selectedRows.includes(i)) {
-<<<<<<< HEAD
-                    item.selected = true
+                    item.selected = true;
                     newItems.push(item);
                 } else {
-                    item.selected = false
-=======
-                    if (item.selected) {
-                        // すでに完了済みのため無視
-                    } else {
-                        item.closed = true;
-                        item.updatedOn = new Date();
-                    }
-
-                    newItems.push(item);
-                } else {
-                    if (item.selected) {
-                        item.closed = false;
-                        item.updatedOn = new Date();
-                    } else {
-                        // もともと未完了のため
-                    }
-
->>>>>>> 1ba4e7a0f29f583854f795181d56b56417d514cc
+                    item.selected = false;
                     newItems.push(item);
                 }
             }
         }
 
-        console.log("newItems")
+        console.log("newItems => ")
         console.log(newItems)
         // ここでstateを変えるとテーブル全体の再描画が開始してチェックが強制的にはずれる...
         this.setState({
             values: newItems
+        })
+    }
+
+    deleteSectedItems() {
+        console.log("deleteSectedItems");
+        this.setState((preState: State, preProps: Props) => {
+            let items = preState.values;
+            items = items.filter(function (item) {
+                return !item.selected;
+            });
+
+            return ({
+                values: items,
+                tableSelectable: false
+            })
+        })
+    }
+
+    deleteItem(num: number) {
+        console.log(`deleteItem(${num}): ${String(this.state.values[num])}`);
+        console.info("Before: ")
+        const newItems: TaskItem[] = [];
+        this.state.values.forEach((value: TaskItem, index: number, array: TaskItem[]) => {
+            console.table(value);
+            if (index !== num) {
+                newItems.push(value);
+            }
+        });
+        console.info("After: ")
+        console.log(newItems)
+
+        this.setState({
+            values: newItems
+        })
+    }
+
+    updateBulkOperationMode() {
+        console.log(`enableBulkOperationMode: ${String(this.state.tableSelectable)} -> ${String(this.state.tableSelectable)}`);
+        this.setState((prevState: State, prevProps: Props) => {
+            return ({
+                tableSelectable: !prevState.tableSelectable
+            });
         })
     }
 
@@ -170,6 +198,10 @@ class ToDoListApp extends Component<Props, State> {
                 <ToDoListTable
                     items={this.state.values}
                     updateSelectedStatus={this.updateSelectedStatus}
+                    deleteSectedItems={this.deleteSectedItems}
+                    updateBulkOperationMode={this.updateBulkOperationMode}
+                    deleteItem={this.deleteItem}
+                    selectable={this.state.tableSelectable}
                 />
 
                 <SimpleDialog
--- src/toDoListApp/ToDoListTable.js
+++ src/toDoListApp/ToDoListTable.js
@@ -1,5 +1,8 @@
 //@flow
 
+// TODO: 
+// Table(material-ui)+Reactで、どうやったらrowのcolumn=-1以外のところをクリックしたときに選択状態になるのを無効にできるか？
+
 
 import * as React from 'react';
 import { PureComponent } from 'react';
@@ -15,11 +18,18 @@ import FlatButton from 'material-ui/FlatButton';
 import { Card, CardHeader } from 'material-ui/Card' // テーブルがボードの上に載っているように表示するために利用
 import DateUtil from 'util/DateUtil';
 import TaskItem from 'model/TaskItem';
-
+/// htmlファイルに次のものを追加してmaterial.ioのものを利用している
+/// <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+///　https://material.io/icons/
+import IconButton from 'material-ui/IconButton';
 
 type Props = {
     items: TaskItem[],
-    updateSelectedStatus: (selectedRows: number[]) => void
+    updateSelectedStatus: (selectedRows: number[] | 'all') => void,
+    deleteSectedItems: () => void,
+    deleteItem: (num: number) => void,
+    updateBulkOperationMode: () => void,
+    selectable: bool,
 };
 
 type State = {
@@ -35,26 +45,64 @@ class ToDoListTable extends PureComponent<Props, State> {
         };
         (this: any).onRowSelection = this.onRowSelection.bind(this);
         (this: any).handleClickBuldDeleteButton = this.handleClickBuldDeleteButton.bind(this);
+        (this: any).handleClickUpdateBulkModeButton = this.handleClickUpdateBulkModeButton.bind(this);
+        (this: any).onCellClick = this.onCellClick.bind(this);
     }
 
-    onRowSelection(selectedRows: number[]) {
-        console.log("Click - onRowSelection:");
-        console.log(selectedRows);
-        console.log(selectedRows.length);
+    onRowSelection(selectedRows: number[] | 'all') {
+        console.log(`Click - onRowSelection: ${String(selectedRows)}[${selectedRows.length} items]`);
 
         this.props.updateSelectedStatus(selectedRows);
         this.setState({
             selectedNum: selectedRows.length
         });
     }
+
+    onCellClick(row: number, column: number): void {
+        console.log("Click - onCellClick[row/column:" + row + "/" + column + "]")
+    }
+
     handleClickBuldDeleteButton() {
         console.log("Click - BuldDeleteButton:" + this.state.selectedNum)
+        this.props.deleteSectedItems();
+    }
+
+    handleClickUpdateBulkModeButton() {
+        console.log("Click - handleClickUpdateBulkModeButton")
+        this.props.updateBulkOperationMode();
     }
 
     render() {
         console.log("ToDoListTable is rendering...")
         const items = this.props.items;
         console.log(items);
+
+        const HeaderButtons = () => {
+            if (this.props.selectable) {
+                return (
+                    <div>
+                        <FlatButton
+                            label="一括削除"
+                            disabled={this.state.selectedNum === 0}
+                            onClick={this.handleClickBuldDeleteButton}
+                        />
+                        <FlatButton
+                            label="解除"
+                            onClick={this.handleClickUpdateBulkModeButton}
+                        />
+                    </div>
+                )
+            } else {
+                return (
+                    <FlatButton
+                        label="一括操作"
+                        onClick={this.handleClickUpdateBulkModeButton}
+                    />
+                )
+            }
+        };
+
+
         const WholeTable = () => (
             <Table
                 fixedHeader={false}
@@ -63,32 +111,50 @@ class ToDoListTable extends PureComponent<Props, State> {
                 multiSelectable     // 複数選択を許容
                 fixedFooter         // フッターを固定
                 onRowSelection={this.onRowSelection}
+                onCellClick={this.onCellClick}
+                selectable={this.props.selectable}
             >
-                <TableHeader>
+                <TableHeader displaySelectAll={true}>
                     <TableRow>
-                        <TableHeaderColumn colSpan="3" tooltip="Super Header" style={{ textAlign: 'right' }}>
-                            <FlatButton
-                                label="一括削除"
-                                disabled={this.state.selectedNum === 0}
-                                onClick={this.handleClickBuldDeleteButton}
-                            />
+                        <TableHeaderColumn colSpan="4" tooltip="Super Header" style={{ textAlign: 'right' }}>
+                            <HeaderButtons />
                         </TableHeaderColumn>
                     </TableRow>
                     <TableRow>
                         {/* <TableHeaderColumn style={{ width: "10%" }}>ID</TableHeaderColumn> */}
                         <TableHeaderColumn style={{ width: "50%" }}> Name</TableHeaderColumn>
-                        <TableHeaderColumn style={{ width: "40%" }}> Created</TableHeaderColumn>
+                        <TableHeaderColumn style={{ width: "20%" }}> Created</TableHeaderColumn>
+                        <TableHeaderColumn style={{ width: "20%" }}> Updated</TableHeaderColumn>
+                        <TableHeaderColumn style={{ width: "10%" }}></TableHeaderColumn>
                     </TableRow>
                 </TableHeader>
-                <TableBody>
-                    {items.map((item) =>
+                <TableBody
+                    deselectOnClickaway={false} // テーブル外をクリックしたら選択状態を全て解除するか？：default=true
+                    showRowHover={true}         // カーソルをテーブルに移動するとその行がハイライトされるか？：default=false
+                    stripedRows={false}         // １行ごとに背景色を変えて見やすくする機能をどうするか？：default=false
+                >
+                    {items.map((item, num) =>
                         <TableRow
                             key={item.id}
-                            selected={item.closed}
+                            selected={item.selected}
+                            striped={true}
+
                         >
                             {/* <TableRowColumn>{item.id}</TableRowColumn> */}
                             <TableRowColumn>{item.value}</TableRowColumn>
                             <TableRowColumn>{DateUtil.getLongDate(item.createdOn)}</TableRowColumn>
+                            <TableRowColumn>{DateUtil.getLongDate(item.updatedOn)}</TableRowColumn>
+                            <TableRowColumn>
+                                <IconButton
+                                    iconClassName="material-icons"
+                                    onClick={(event: any) => {
+                                        console.log(`IconButton's click!: [${num}]${item.value}(${item.id})`)
+                                        this.props.deleteItem(num)
+                                    }}
+                                    disabled={this.props.selectable}>
+                                    delete_forever
+                                </IconButton>
+                            </TableRowColumn>
                         </TableRow>
                     )}
                 </TableBody>
